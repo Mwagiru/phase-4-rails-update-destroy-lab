8575f2c9f9c7686bd760f359ab582fb2241031bd
Mwagiru <joymwagiru@gmail.com>
2022-07-21 11:37:50 +0300

    commit tree d13db9f0a6ed2032f20e2e41103b94f3165f5f49

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,6 +17,17 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+    def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+  
+      plant.destroy
+      head :no_content
+  end
 
   private
 
@@ -24,3 +35,4 @@ class PlantsController < ApplicationController
     params.permit(:name, :image, :price, :is_in_stock)
   end
 end
+
